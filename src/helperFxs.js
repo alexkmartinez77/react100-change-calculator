cf0bec606781234c1d02e186a7932fcdb7fca805
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-12 14:54:00 -0700

    OnClick calculates denomination quantity using helper funciton and updates values to state.

---
+++ src/helperFxs.js
@@ -0,0 +1,14 @@
+//Called on by calculate button to calculate each monetary change value and display it
+const calculateChange = (obj) => {
+
+  let {due, received, changeObjects, show} = obj;
+  var remainingChange = received - due;
+  
+  return changeObjects.map(currency => {
+    currency.quantity = Math.floor(remainingChange / currency.value);
+    currency.quantity > 0 ? currency.show = true : currency.show = false;
+    remainingChange = (remainingChange % currency.value).toFixed(2);
+  });
+
+}
+  export default calculateChange;
